465ce832f226c57ebacd57ca1c7e1beda8f6602d
Danielid Ramirez <danielid132@gmail.com>
2021-04-15 13:22:17 -0400

    Modificando método numero_de_cuenta

--- cuenta_bancaria.rb
+++ cuenta_bancaria.rb
@@ -1,23 +1,29 @@
 class CuentaBancaria
 
     attr_accessor :nombre_usuario
+    attr_reader :vip, :numero_de_cuenta
 
     def initialize(nombre_usuario, numero_de_cuenta, vip = 0)
         @nombre_usuario = nombre_usuario
-        @vip = vip
-        digitos = numero_de_cuenta.digits
-        if digitos.count==8
+
+        if numero_de_cuenta.digits.count==8
             @numero_de_cuenta = numero_de_cuenta
+            if vip == 0 || vip == 1
+                @vip = vip
+            else
+                raise RangeError.new('Vip debe ser 1 ó 0')
+            end
         else
             raise RangeError.new('Deben ser 8 digitos')
         end
     end
 
-    def numero_de_cuenta()
-        if @vip==0
-            @numero_de_cuenta = "0-#{@numero_de_cuenta}"
+    def numero_de_cuenta
+        if self.vip==0
+            return "0-#{@numero_de_cuenta}"
         else
-            @numero_de_cuenta = "1-#{@numero_de_cuenta}"
+            return "1-#{@numero_de_cuenta}"
         end
     end
 end
+
